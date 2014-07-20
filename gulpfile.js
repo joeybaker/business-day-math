739a91d173cdf202a6a50d5f0b7241557e085880
Joey Baker <joey@byjoeybaker.com>
2014-07-20 13:48:51 -0700

    Update gulpfile & dev deps

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,8 @@ var gulp = require('gulp')
   , jscs = require('gulp-jscs')
   , git = require('gulp-git')
   , todo = require('gulp-todo')
-  , mocha = require('gulp-mocha')
   , argv = require('minimist')(process.argv.slice(2)) || {}
-  , bump = require('gulp-bump')
+  , runSequence = require('run-sequence')
   , paths = {
     app: ['./lib/**/*.js', './index.js']
     , tests: ['./test/**/*.js']
@@ -36,51 +35,75 @@ gulp.task('lint', function(){
     }))
 })
 
-gulp.task('test', ['lint'], function(){
-  return gulp.src('test/**/*.js')
-    .pipe(cache('test'))
-    .pipe(mocha({
-      ui: 'bdd'
-      , reporter: 'dot'
-    }))
+gulp.task('test', function(done){
+  require('child_process').spawn('npm', ['test'], {stdio: 'inherit', cwd: __dirname})
+    .on('close', done)
 })
 
-gulp.task('bump', ['gitPull', 'test'], function(){
-  return gulp.src('./package.json')
-    .pipe(bump({
-      type: argv.bump || 'patch'
-      , indent: 2
-    }))
-    .pipe(gulp.dest('./'))
+gulp.task('version', function(done){
+  require('child_process').spawn('npm', [
+    'version'
+    , argv.bump || 'patch'
+    , '-m "%s"'
+  ], {stdio: 'inherit', cwd: __dirname})
+    .on('close', done)
 })
 
 gulp.task('gitPrep', function(done){
-  require('child_process').spawn('sh', ['./sh/git/isclean.sh'], {cwd: __dirname, stdio: 'inherit'})
-    .on('close', done)
+  var isClean = 'function require_clean_work_tree(){\n'
+    + '  # Update the index\n'
+    + '  git update-index -q --ignore-submodules --refresh\n'
+    + '  err=0\n'
+    + '\n'
+    + '  # Disallow unstaged changes in the working tree\n'
+    + '  if ! git diff-files --quiet --ignore-submodules --\n'
+    + '  then\n'
+    + '      echo >&2 "You have unstaged changes."\n'
+    + '      git diff-files --name-status -r --ignore-submodules -- >&2\n'
+    + '      err=1\n'
+    + '  fi\n'
+    + '\n'
+    + '  # Disallow uncommitted changes in the index\n'
+    + '  if ! git diff-index --cached --quiet HEAD --ignore-submodules --\n'
+    + '  then\n'
+    + '      echo >&2 "Your index contains uncommitted changes."\n'
+    + '      git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2\n'
+    + '      err=1\n'
+    + '  fi\n'
+    + '\n'
+    + '  if [ $err = 1 ]\n'
+    + '  then\n'
+    + '      echo >&2 "Please commit or stash them."\n'
+    + '      exit 1\n'
+    + '  fi\n'
+    + '}\n'
+    + 'require_clean_work_tree\n'
+
+  require('child_process').exec(isClean, {cwd: __dirname}, done)
 })
 
-gulp.task('gitPull', ['gitPrep'], function(done){
+gulp.task('gitPull', function(done){
   git.pull('origin', 'master', {args: '--rebase'}, done)
 })
 
-gulp.task('gitCommit', ['bump'], function(){
-  var pkg = require('./package.json')
-
-  return gulp.src('./package.json')
-    .pipe(git.commit(pkg.version))
+gulp.task('gitPush', function(done){
+  return git.push('origin', 'master', {args: '--tags'}, done)
+    .end()
 })
 
-gulp.task('tag', ['gitCommit'], function(done){
-  var pkg = require('./package.json')
-
-  git.tag('v' + pkg.version, pkg.version, null, done)
-})
-
-gulp.task('gitPush', ['tag'], function(done){
-  git.push('origin', 'master', {args: '--tags'}, done)
-})
-
-gulp.task('publish', ['gitPush'], function(done){
+gulp.task('npmPublish', function(done){
   require('child_process').spawn('npm', ['publish'], {stdio: 'inherit', cwd: __dirname})
     .on('close', done)
+})
+
+gulp.task('publish', function(done){
+  runSequence(
+    'gitPrep'
+    , 'gitPull'
+    , 'gitPrep'
+    , ['lint', 'test']
+    , 'version'
+    , ['gitPush', 'npmPublish']
+    , done
+  )
 })
